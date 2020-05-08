33285f91a4e54a5517c4d4e2dc61b075a887a593
MK <khalkhal@ualberta.ca>
2020-05-08 17:03:16 -0600

    dpdata

--- Phase2/Test_Steinhardt/test_seinhardt.py
+++ Phase2/Test_Steinhardt/test_seinhardt.py
@@ -1,9 +1,16 @@
 from Phase2.Bond_code import utils
 from pathlib import Path
+import dpdata
 
-atoms, cell = utils.read_poscar('POSCAR')
-atoms = utils.CN(atoms, cell)
-atoms = utils.steinhardt(atoms, cell, 2.55, [4, 6, 8, 10])
-utils.write_lammps(Path('./'), atoms, cell)
-print(atoms)
+# atoms, cell = utils.read_xsd('Millerite-Rlx.xsd')
+# atoms = utils.CN(atoms, cell)
+# atoms = utils.steinhardt(atoms, cell, 2.55, [4, 6, 8, 10])
+# utils.write_lammps(Path('./'), atoms, cell)
+
+d_poscar = dpdata.System('POSCAR', fmt = 'vasp/poscar')
+d_poscar.to('lammps/lmp', 'conf.lmp', frame_idx=0)
+
+d_lmp = dpdata.System('conf.lmp', fmt = 'lammps/lmp')
+d_lmp.to('vasp/poscar', 'poscar1', frame_idx=0)
+# print(atoms)
 
