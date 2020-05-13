a3a81c0913bf825ad297d2810d0a64bccf7a0e6c
MK <khalkhal@ualberta.ca>
2020-05-13 16:14:22 -0600

    new surface_2D.h5

--- Phase2/Steinhadt/surface_example_maker.py
+++ Phase2/Steinhadt/surface_example_maker.py
@@ -7,13 +7,28 @@ from Phase2.Bond_code import utils
 #Energy per NiS in Millerite unitcell
 eng_NiS = float(-93.110682/9)
 
-print("reading vasp files to build the test set ...")
-# test_folder = Path('/home/khalkhal/Simulations/VASP/Millerite/Machine_Learning/DataSet/Test/VASP_files'
-# )
+def sinle_example(atoms, temp):
+    atom_num = len(atoms)
+    # ids = [i for i in range(len(atoms)) if atoms[i][3] != 53 and atoms[i][3] != 132]  # find surface atoms
+    # uc_num = len(ids)
+    # we are only interested in order parameters of atoms on (or near) surface
+    # q4 = [atoms[i][5][0] for i in ids]
+    # q6 = [atoms[i][5][1] for i in ids]
+    q4 = [x[5][0] for x in atoms]
+    q6 = [x[5][1] for x in atoms]
+    # calculate the 2D histogram
+    H, xedges, yedges = np.histogram2d(q4, q6, bins=(100, 100), range=[[0.1, 0.8], [0.1, 0.8]])
+    x = np.divide(H.T, atom_num)  # normalizing the mesh to the total number of atoms
+    # plt.imshow(H, interpolation='nearest', origin='low', extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+    # plt.show()
+    # this is the energy difference between our structure and bulk.
+    eng = float(temp)
+    y = 2 * eng / atom_num - eng_NiS
+
+    return x, y
+
 address = Path('/home/khalkhal/Simulations/VASP/Millerite/Surfaces/Initial_Energy')
-
 struct_num = -1
-
 print("reading vasp files to build the training set ...")
 counter = 0
 xlist = []
@@ -31,22 +46,9 @@ for dir_path in os.listdir(address):
             atom_num = len(atoms)
             atoms = utils.CN(atoms, cell)
             atoms = utils.steinhardt(atoms, cell, 2.55, [4, 6, 8, 10])
-            ids = [i for i in range(len(atoms)) if atoms[i][3] != 53 and atoms[i][3] != 132] # find surface atoms
-            uc_num = len(ids)
-            # we are only interested in order parameters of atoms on (or near) surface
-            q4 = [atoms[i][5][0] for i in ids]
-            q6 = [atoms[i][5][1] for i in ids]
-            # calculate the 2D histogram
-            H, xedges, yedges = np.histogram2d(q4, q6, bins=(100, 100), range=[[0.1, 0.8], [0.1, 0.8]])
-            H = H.T # normalizing the mesh to the total number of atoms
-            # plt.imshow(H, interpolation='nearest', origin='low', extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
-            # plt.show()
-
-            # this is the energy difference between our structure and bulk.
-            eng_bar = 2*eng/atom_num - eng_NiS
-
-            xlist.append(H)
-            ylist.append(eng_bar)
+            x, y = sinle_example(atoms, eng)
+            xlist.append(x)
+            ylist.append(y)
 x = np.array(xlist)
 y = np.array(ylist)
 
